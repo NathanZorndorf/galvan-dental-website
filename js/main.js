fe39b798e022119b06e27c5002c9698d8335a556
Nathan Zorndorf <nzorndorf@gmail.com>
2016-10-18 21:49:24 -0700

    In the middle of working on Our Office section, adding the hours in particular. Its challenging because I need the divs to lay next to each other horizontally when viewing on desktop, then stack vertically when viewing on phone.

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 		}
 	});
     */
-    
+
 	// Fancybox
 	$('.work-box').fancybox();
 
@@ -36,6 +36,8 @@ $(document).ready(function() {
 	    	}
 	  	});
 	});
+    
+    
     
 	nav.find('a').on('click', function () {
 	  	var $el = $(this)
