09597a76039d33750ed40f3cacbe3c8e985b055c
Nathan Zorndorf <nzorndorf@gmail.com>
2016-10-21 15:57:23 -0700

    Fixed issue where Doctors section was bleeding into the section above (which was gallery). The issue was that the gallery section had a fixed height, so i removed the fixed height, and the bleed issue went away! and nothing else seemed to break. :)

--- js/main.js
+++ js/main.js
@@ -37,6 +37,93 @@ $(document).ready(function() {
 	  	});
 	});
     
+    // jQuery animations on page scroll    
+    $('#element-to-animate-left').css('opacity', 0); // Have the element start out invisible 
+    $('#element-to-animate-right').css('opacity', 0); // Have the element start out invisible 
+    $('#element-to-animate-up').css('opacity', 0); // Have the element start out invisible 
+    
+    $("#element-to-animate-about-left").waypoint(function() 
+    {                
+        $('#element-to-animate-about-left').addClass('fadeInLeft');                        
+    }, {offset: '100%'});
+    
+    $("#element-to-animate-about-right").waypoint(function() 
+    {                      
+        $('#element-to-animate-about-right').addClass('fadeInRight');              
+    }, {offset: '100%'});
+    
+    $("#element-to-animate-about-up").waypoint(function() 
+    {                                             
+        $('#element-to-animate-about-up').addClass('fadeInUp');                       
+    }, {offset: '100%'});
+    
+    $("#element-to-animate-about-up-2").waypoint(function() 
+    {                                             
+        $('#element-to-animate-about-up-2').addClass('fadeInUp');                       
+    }, {offset: '100%'});
+    
+    $("#element-to-animate-services-up-1").waypoint(function() 
+    {                                             
+        $('#element-to-animate-services-up-1').addClass('fadeInUp');                       
+    }, {offset: '100%'});
+    
+    $("#element-to-animate-services-up-2").waypoint(function() 
+    {                                             
+        $('#element-to-animate-services-up-2').addClass('fadeInUp');                       
+    }, {offset: '100%'});
+    
+    $("#element-to-animate-services-up-3").waypoint(function() 
+    {                                             
+        $('#element-to-animate-services-up-3').addClass('fadeInUp');                       
+    }, {offset: '100%'});
+
+    $("#element-to-animate-doctors-up-1").waypoint(function() 
+    {                                             
+        $('#element-to-animate-services-up-1').addClass('fadeInUp');                       
+    }, {offset: '100%'});
+
+    $("#element-to-animate-doctors-up-2").waypoint(function() 
+    {                                             
+        $('#element-to-animate-services-up-2').addClass('fadeInUp');                       
+    }, {offset: '100%'});
+    
+    $("#element-to-animate-doctors-up-3").waypoint(function() 
+    {                                             
+        $('#element-to-animate-services-up-3').addClass('fadeInUp');                       
+    }, {offset: '100%'});
+
+    $("#element-to-animate-doctors-down").waypoint(function() 
+    {                                             
+        $('#element-to-animate-services-down').addClass('fadeInDown');                       
+    }, {offset: '100%'});
+    
+    /*
+    var $animation_elements = $('.animation-element');
+    var $window = $(window);
+    
+    $window.on('scroll', check_if_in_view);
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
+    
+    function check_if_in_view() {
+        var window_height = $window.height();
+        var window_top_position = $window.scrollTop();
+        var window_bottom_position = (window_top_position + window_height);
+        
+        $.each($animation_elements, function() {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+            
+            //check to see if the current container is in the viewport 
+            if((element_bottom_position >= window_top_position) &&
+               (element_top_position <= window_bottom_position) {
+                    $element.addClass('.in-view');
+            }                          
+        });
+    }
+    */
     
     
 	nav.find('a').on('click', function () {
